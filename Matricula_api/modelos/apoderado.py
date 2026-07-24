598893529cb6a8da61840b5d2750dfb477416102
anaperu1972 <ana.ciezac@istpargentina.edu.pe>
2026-07-24 14:53:36 -0500

    Mejora de las clases Estudiante, aplicando buenas prácticas de programación

--- Matricula_api/modelos/apoderado.py
+++ Matricula_api/modelos/apoderado.py
@@ -1,13 +1,20 @@
 from modelos.persona import Persona
 
 class Apoderado(Persona):
+     """
+    Clase que representa a un Apoderado del sistema.
+    Hereda los atributos básicos de la clase Persona.
+    """
+    
+def __init__(self, dni, nombres, apellidos, telefono, email):
+        # Llama al constructor de la clase padre (Persona)
+    super().__init__(dni, nombres, apellidos, telefono, email)
+    self.id = None   # Identificador único que se asignará en la base de datos
+        
+def obtener_descripcion(self):
+        """Devuelve una descripción simple del apoderado."""
+        return f"Apoderado: {self.nombres} {self.apellidos}" 
 
-    def __init__(self, dni, nombres, apellidos, telefono, email):
-        super().__init__(dni, nombres, apellidos, telefono, email)
-        self.id = None
-
-    def obtener_descripcion(self):
-        return f"Apoderado: {self.nombres} {self.apellidos}"
-
-    def __str__(self):
+def __str__(self):
+        """Formatea la representación en texto del objeto."""
         return f"[{self.id}] {self.nombres} {self.apellidos} | DNI: {self.dni}"
--- Matricula_api/modelos/estudiante.py
+++ Matricula_api/modelos/estudiante.py
@@ -1,10 +1,15 @@
 from modelos.persona import Persona
 
 class Estudiante(Persona):
+    """
+    Representa a un estudiante del sistema de matrícula.
+    Hereda los atributos de la clase Persona y agrega
+    información propia del estudiante.
+    """
 
     def __init__(self, dni, nombres, apellidos, telefono, email,
                  fecha_nac, direccion, apoderado):
-
+        """Inicializa un nuevo estudiante."""
         super().__init__(dni, nombres, apellidos, telefono, email)
 
         self.id = None
@@ -13,9 +18,12 @@ class Estudiante(Persona):
         self.apoderado = apoderado
 
     def obtener_descripcion(self):
+        """Devuelve una descripción breve del estudiante."""
+        
         return f"Estudiante: {self.nombres} {self.apellidos}"
-
+        
     def __str__(self):
-        return (f"[{self.id}] {self.nombres} {self.apellidos} | "
+        """Devuelve una representación legible del estudiante."""
+        return (f"[{self.id}] {self.nombres} {self.apellidos} |"
                 f"DNI: {self.dni} | "
                 f"Apoderado: {self.apoderado.nombres} {self.apoderado.apellidos}")
