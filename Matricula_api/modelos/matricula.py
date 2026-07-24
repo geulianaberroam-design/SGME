43a5b74ac41c9deaca4fb04bf5931672a2db72b8
anaperu1972 <ana.ciezac@istpargentina.edu.pe>
2026-07-24 18:08:32 -0500

    Se optimiza la clase Matricula agregando documentación y mejoras de formato

--- Matricula_api/modelos/matricula.py
+++ Matricula_api/modelos/matricula.py
@@ -1,16 +1,27 @@
 class Matricula:
+    """Representa una matricula de un estudiantes."""
 
     def __init__(self, anio, fecha, estado, estudiante, grado_seccion):
-        self.id = None
-        self.anio = anio
-        self.fecha = fecha
-        self.estado = estado
-        self.estudiante = estudiante
-        self.grado_seccion = grado_seccion
-
+       
+         """ Inicializa una nueva matricula."""
+         
+         self.id = None
+         self.anio = anio
+         self.fecha = fecha
+         self.estado = estado.strip().title()
+         self.estudiante = estudiante
+         self.grado_seccion = grado_seccion
+        
+    def obtener_resumen(self):
+         """Devuelve un resumen de matricula."""
+        return(f"{self.estudiantes.nombres} {self.estudiantes.apellidos}")
+    
     def __str__(self):
+        """Devuelve una representación legible de la matrícula."""
         return (f"[{self.id}] "
                 f"Año: {self.anio} | "
+                f"Fecha:{self.fecha} |"
+                f"Fecha:{self.estado} |"
                 f"Estudiante: {self.estudiante.nombres} {self.estudiante.apellidos} | "
                 f"Grado: {self.grado_seccion.grado} "
                 f"Sección: {self.grado_seccion.seccion}")
