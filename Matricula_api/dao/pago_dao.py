acea70f3e9bb5e6956f38bac08070632d4be2ea0
anaperu1972 <ana.ciezac@istpargentina.edu.pe>
2026-07-25 15:08:13 -0500

    Mejora de la clase PagoDAO agregando documentación y buenas prácticas

--- Matricula_api/dao/pago_dao.py
+++ Matricula_api/dao/pago_dao.py
@@ -2,14 +2,23 @@ from modelos.pago import Pago
 
 
 class PagoDAO:
+    """
+    Gestiona las operaciones CRUD de los pagos
+    almacenados en memoria.
+    """
 
     def __init__(self):
+        """
+        Inicializa la lista de pagos y el contador de identificadores.
+        """
         self.__bd = []
         self.__cid = 1
 
     # CREATE
     def insertar(self, pago):
-
+        """
+        Registra un nuevo pago.
+        """
         pago.id = self.__cid
         self.__cid += 1
 
@@ -19,11 +28,16 @@ class PagoDAO:
 
     # READ ALL
     def obtener_todos(self):
+        """
+        Devuelve la lista de todos los pagos registrados.
+        """
         return self.__bd
 
     # READ
     def buscar_por_id(self, id):
-
+        """
+        Busca un pago por su identificador.
+        """
         for pago in self.__bd:
             if pago.id == id:
                 return pago
@@ -32,25 +46,26 @@ class PagoDAO:
 
     # UPDATE
     def actualizar(self, id, monto, comprobante):
-
+        """
+        Actualiza el monto y el comprobante de un pago.
+        """
         pago = self.buscar_por_id(id)
 
         if pago:
-
             pago.monto = monto
             pago.comprobante = comprobante
-
             return True
 
         return False
 
     # DELETE
     def eliminar(self, id):
-
+        """
+        Elimina un pago por su identificador.
+        """
         pago = self.buscar_por_id(id)
 
         if pago:
-
             self.__bd.remove(pago)
             return True
 
@@ -58,4 +73,7 @@ class PagoDAO:
 
     # COUNT
     def total(self):
+        """
+        Devuelve la cantidad total de pagos registrados.
+        """
         return len(self.__bd)
