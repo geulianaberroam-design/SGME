df4aa2e4cf357fc8e5e80603690de886fb754b7d
anaperu1972 <ana.ciezac@istpargentina.edu.pe>
2026-07-25 14:28:47 -0500

    Mejora de la clase DocumentoDAO agregando documentación y buenas prácticas

--- Matricula_api/dao/documento_dao.py
+++ Matricula_api/dao/documento_dao.py
@@ -2,14 +2,23 @@ from modelos.documento import Documento
 
 
 class DocumentoDAO:
+    """
+    Gestiona las operaciones CRUD de los documentos
+    almacenados en memoria.
+    """
 
     def __init__(self):
+        """
+        Inicializa la lista de documentos y el contador de IDs.
+        """
         self.__bd = []
         self.__cid = 1
 
     # CREATE
     def insertar(self, documento):
-
+        """
+        Registra un nuevo documento.
+        """
         documento.id = self.__cid
         self.__cid += 1
 
@@ -19,11 +28,16 @@ class DocumentoDAO:
 
     # READ ALL
     def obtener_todos(self):
+        """
+        Devuelve la lista de todos los documentos registrados.
+        """
         return self.__bd
 
     # READ
     def buscar_por_id(self, id):
-
+        """
+        Busca un documento por su identificador.
+        """
         for documento in self.__bd:
             if documento.id == id:
                 return documento
@@ -32,32 +46,33 @@ class DocumentoDAO:
 
     # UPDATE
     def actualizar(self, id, estado):
-
+        """
+        Actualiza el estado de un documento.
+        """
         documento = self.buscar_por_id(id)
 
         if documento:
-
             documento.estado = estado
-
             return True
 
         return False
 
     # DELETE
     def eliminar(self, id):
-
+        """
+        Elimina un documento por su identificador.
+        """
         documento = self.buscar_por_id(id)
 
         if documento:
-
             self.__bd.remove(documento)
-
             return True
 
         return False
 
     # COUNT
     def total(self):
+        """
+        Devuelve la cantidad total de documentos registrados.
+        """
         return len(self.__bd)
-    
-#documento_dao parte ana
